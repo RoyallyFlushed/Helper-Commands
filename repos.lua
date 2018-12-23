59db04bf2418a64035a4fdd2b9ff842eda83622d
RoyallyFlushed <36049292+RoyallyFlushed@users.noreply.github.com>
2018-12-23 23:02:57 +0000

    Create repos.lua

---
+++ repos.lua
@@ -0,0 +1,26 @@
+
+
+--// maps a given range from a specific iterator to a new range
+local function map(n, start, stop, newStart, newStop, withinBounds)
+    local value = ((n - start) / (stop - start)) * (newStop - newStart) + newStart
+    
+    --// Returns basic value
+    if not withinBounds then
+        return value
+    end
+    
+    --// Returns values constrained to exact range
+    if newStart < newStop then
+        return math.max(math.min(value, newStop), newStart)
+    else
+        return math.max(math.min(value, newStart), newStop)
+    end
+end
+
+print(map(12, 50, 100, 2, 10, true))
+
+for i = 0, 600 do
+    print(map(i, 0, 600, 0, 255, true))
+    -->> Outputs the range 0-600 mapped between 0-255
+end
+
